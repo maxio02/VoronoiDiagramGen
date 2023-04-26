aa4edc1e1e672fb09928239f8c67e2cfe75cc0a0
Szymon Maksymowicz <szymonmaksymowicz2002@gmail.com>
2023-04-26 14:07:05 +0200

    GUI and parabola calculations

---
+++ bst.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+
+    def find(self, value):
+        if value == self.value:
+            return self
+        elif value < self.value and self.left is not None:
+            return self.left.find(value)
+        elif value > self.value and self.right is not None:
+            return self.right.find(value)
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 from dataclasses import dataclass
 import random
-
+import heapq
+from bst import Node
 WIDTH = 1600
 HEIGHT = 900
+line_y = 1
+points = []
 
 
 @dataclass
@@ -19,7 +22,22 @@ class edge:
     p2: point
 
 
+class arc:
+    def __init__(self, focus: point, apex: point):
+        self.focus = focus
+        self.apex = apex
+
+    # apex (h, k)
+    # focus (h, k + p)
+    def f(self, x):
+        h = self.apex.x
+        k = self.apex.y
+        p = self.focus.y-k
+        return ((h**2-2*h*x+4*k*p+x**2)/4/p)
+
+
 def generate_points(n: int) -> list[point]:
+    global points
     points = []
     for i in range(n):
         points.append(point(random.randint(15, WIDTH-15),
@@ -37,6 +55,16 @@ def draw_edges(edges: list[edge]):
     for edge in edges:
         canvas.create_line(edge.p1.x, edge.p1.y, edge.p2.x,
                            edge.p2.y, fill='black')
+        
+
+def update_line(liney):
+    global line_y 
+    line_y = int(liney)
+    draw(points)
+
+
+def draw_line():
+    canvas.create_line(0,line_y,WIDTH,line_y)
 
 
 def generate_all_edges(points: list[point]) -> list[edge]:
@@ -46,31 +74,92 @@ def generate_all_edges(points: list[point]) -> list[edge]:
             edges.append(edge(point1, point2))
     return edges
 
-def generate_new_pattern_diagram():
-    
+
+def calc_parabolas(points:list[point], line: int):
+    parabolas = []
+    for pt in points:
+        parabolas.append(arc(pt, point(pt.x, pt.y-(pt.y-line), 1)))
+    return parabolas
+
+
+def draw_parabolas(parabolas:list[arc]):
+    for parabola in parabolas:
+        if parabola.focus.y < line_y:
+            for i in range(WIDTH):
+                x = i
+                y = parabola.f(x)
+                if y > 0 | i%2==0:
+                    canvas.create_line(x, y, x+2, parabola.f(x+2), fill='black')
+
+
+def draw(points):
     canvas.delete('all')
-    points = generate_points(int(entry.get()))
-    edges = generate_all_edges(points)
     draw_points(points)
-    draw_edges(edges)
+    parabolas = calc_parabolas(points, line_y)
+    draw_parabolas(parabolas)
+    draw_line()
+
+def sort_points_by(points: list[point], axis: str) -> list[point]:
+    match axis:
+        case 'x':
+            points.sort(key=lambda p: p.x)
+        case 'y':
+            points.sort(key=lambda p: p.y)
+
+    return points
+
+
+
+
+def generate_new_pattern_diagram():
+    points = generate_points(int(entry.get()))
+    points = sort_points_by(points, 'x')
+
+    draw(points)
+    
+
+    '''Q = []
+        for point in points:
+            heapq.heappush(Q,'site')
+
+        beachline = Node(0)
+
+
+        while Q:
+            event = heapq.heappop()
+             match event:
+                case 'site':
+
+                case 'vertex':
+
+        edges = generate_all_edges(points)'''
+
+
+    # Loop through the range of x values, plot each point on the canvas
+    draw(points)
+    # draw_edges(edges)
     canvas.pack()
 
+
 window = tk.Tk()
+
 
 
 canvas = tk.Canvas(window, width=1600, height=900)
 canvas.pack()
 
 
+button = tk.Button(window, text="Generate new pattern",
+                   command=generate_new_pattern_diagram)
+button.place(x=WIDTH-button.winfo_reqwidth()-10,
+             y=HEIGHT-button.winfo_reqheight()-10)
 
-button = tk.Button(window, text="Generate new pattern", command=generate_new_pattern_diagram)
-button.place(x=WIDTH-button.winfo_reqwidth()-10, y=HEIGHT-button.winfo_reqheight()-10)
+entry = tk.Entry(window, width=5)
 
-entry = tk.Entry(window,width=5)
+entry.place(x=WIDTH-button.winfo_x()-button.winfo_reqwidth() -
+            50, y=HEIGHT-entry.winfo_reqheight()-10)
 
-entry.place(x=WIDTH-button.winfo_x()-button.winfo_reqwidth()-50, y=HEIGHT-entry.winfo_reqheight()-10)
-
+slider = tk.Scale(window, from_=0, to=HEIGHT,length=400, orient=tk.HORIZONTAL, command=update_line)
+slider.pack()
 
 window.mainloop()
-
-
